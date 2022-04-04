6c1c16aaf86433508b004ded327867214b1595c5
Lucas Oliveira <lucasolidev@gmail.com>
2022-04-04 05:34:19 -0300

    feito

--- 1-Exerc_PythonBrasil_EstruturaSequencial/Exc_18_Python_Brasil.py
+++ 1-Exerc_PythonBrasil_EstruturaSequencial/Exc_18_Python_Brasil.py
@@ -1,2 +1,12 @@
 """18 - Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de 
 Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos)."""
+
+tamanho_arquivo = float(input('Qual o tamanho do arquivo em MB? '))
+velocidade_link = float(input('Qual a velocidade da internet em Mbps? '))
+
+segundos = tamanho_arquivo/velocidade_link
+minutos = int(segundos/60)
+print(minutos)
+segundos = segundos % 60
+print(segundos)
+print(f"Tempo aproximado para download: {minutos} minutos e {segundos:.2f} segundos")
