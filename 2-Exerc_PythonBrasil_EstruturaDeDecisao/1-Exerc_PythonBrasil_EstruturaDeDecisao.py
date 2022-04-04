1ca44fe57e9c3a62ba977e947e68121c7ad6e0fe
Lucas Oliveira <lucasolidev@gmail.com>
2022-04-04 06:21:56 -0300

    Feito

---
+++ 2-Exerc_PythonBrasil_EstruturaDeDecisao/1-Exerc_PythonBrasil_EstruturaDeDecisao.py
@@ -0,0 +1,9 @@
+# 1-Faça um Programa que peça dois números e imprima o maior deles.
+
+numero1 = int(input('Digite o primeiro numero '))
+numero2 = int(input('Digite o segundo numero '))
+
+if numero1 > numero2:
+    print(numero1)
+else:
+    print(numero2)
